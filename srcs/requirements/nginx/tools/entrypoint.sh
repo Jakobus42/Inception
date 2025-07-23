bd2d862f1b9c6615690466439c89e8e5f82551c3
Jakobus42 <jakob.sadjina@icloud.com>
2025-07-23 17:56:43 +0200

    feat(nginx): better security

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# TODO: maybe get all NGINX vars out of env?
-NGINX_HOST="${NGINX_HOST:-jsadjina.42.fr}" \
-NGINX_PORT="${NGINX_PORT:-443}" \
-envsubst '${NGINX_HOST} ${NGINX_PORT}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+# TODO: what should even be in env?
+
+if [ "$1" = 'nginx' ]; then
+    NGINX_HOST="${NGINX_HOST:-jsadjina.42.fr}" \
+    envsubst '${NGINX_HOST}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+
+    exec "$@"
+fi
 
 exec "$@"
