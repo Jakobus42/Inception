eb3623a384787848768d1d195e846b94502902c4
Jakobus42 <jakob.sadjina@icloud.com>
2025-07-23 15:42:40 +0200

    feat(nginx): fast cgi for wordpress

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+# TODO: maybe get all NGINX vars out of env?
 NGINX_HOST="${NGINX_HOST:-jsadjina.42.fr}" \
 NGINX_PORT="${NGINX_PORT:-443}" \
-envsubst < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${NGINX_HOST} ${NGINX_PORT}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
-exec nginx -g "daemon off;"
+exec "$@"
