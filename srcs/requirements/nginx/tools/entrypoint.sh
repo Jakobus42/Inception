f9a47c500c9a8ba40cde67a708c980f92b79b697
Jakobus42 <jakob.sadjina@icloud.com>
2025-07-30 12:18:57 +0200

    style: foramt

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -2,11 +2,7 @@
 
 # TODO: what should even be in env?
 
-if [ "$1" = 'nginx' ]; then
-    NGINX_HOST="${NGINX_HOST:-jsadjina.42.fr}" \
-    envsubst '${NGINX_HOST}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
-
-    exec "$@"
-fi
+NGINX_HOST="${NGINX_HOST:-jsadjina.42.fr}" \
+  envsubst '${NGINX_HOST}' </etc/nginx/templates/nginx.conf.template >/etc/nginx/nginx.conf
 
 exec "$@"
