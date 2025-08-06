7a9d34b23439ddc575e0be0df1a329dfe72074a1
Jakobus42 <jakob.sadjina@icloud.com>
2025-08-06 16:43:29 +0200

    feat(mariadb): init sql and healthcheck

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ ! -d /var/lib/mysql ]; then
+  mariadb-install-db --datadir=/var/lib/mysql
+
+  DB_PASSWORD=$(cat /run/secrets/db_password)
+  envsubst </tools/init_template.sql >/tmp/init.sql
+  unset DB_PASSWORD
+
+  mariadbd --skip-networking --init-file=/tmp/init.sql
+  rm /tmp/init.sql
+fi
+
+exec "$@"
