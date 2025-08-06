23add65fc015b364ff4e80e89f87e6df12d79df1
Jakobus42 <jakob.sadjina@icloud.com>
2025-08-06 16:46:43 +0200

    style(nginx): rename file for consistency

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# TODO: what should even be in env?
-
-NGINX_HOST="${NGINX_HOST:-jsadjina.42.fr}" \
-  envsubst '${NGINX_HOST}' </etc/nginx/templates/nginx.conf.template >/etc/nginx/nginx.conf
+envsubst '${NGINX_HOST}' </etc/nginx/templates/nginx_template.conf >/etc/nginx/nginx.conf
 
 exec "$@"
