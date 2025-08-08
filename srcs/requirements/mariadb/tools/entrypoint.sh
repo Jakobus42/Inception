8859885790c3dc83d68443120dc6250e2bc61798
Jakobus42 <jakob.sadjina@icloud.com>
2025-08-08 15:32:23 +0200

    feat: just set password in env instaed of secret

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,7 +3,6 @@
 if [ ! -d /var/lib/mysql/mysql ]; then
   mariadb-install-db --datadir=/var/lib/mysql
 
-  DB_PASSWORD=$(cat /run/secrets/db_password)
   envsubst </tools/init_template.sql >/tmp/init.sql
   unset DB_PASSWORD
 
