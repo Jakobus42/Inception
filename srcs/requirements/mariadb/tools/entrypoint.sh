637a9c1d56321405b7e8794718ead405c788e365
Jakobus42 <jakob.sadjina@icloud.com>
2025-08-15 17:51:46 +0200

    feat: set -e for the win

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -d /var/lib/mysql/mysql ]; then
   mariadb-install-db --datadir=/var/lib/mysql
 
   envsubst </tools/init_template.sql >/tmp/init.sql
-  unset DB_PASSWORD
-
   mariadbd --skip-networking --init-file=/tmp/init.sql
   rm /tmp/init.sql
 fi
+
+unset DB_PASSWORD
 
 exec "$@"
--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 envsubst '${NGINX_HOST}' </etc/nginx/templates/nginx_template.conf >/etc/nginx/nginx.conf
 
 exec "$@"
